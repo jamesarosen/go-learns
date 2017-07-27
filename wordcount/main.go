8cb2db7bc8b9e87c811420ca39b2eb91d5ae9d29
James A Rosen <james@fastly.com>
2017-07-27 15:52:30 -0700

    simplify wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -11,12 +11,7 @@ func main() {
 
 	// adapted from https://golang.org/pkg/bufio/#example_Scanner_custom
 	scanner := bufio.NewScanner(os.Stdin)
-
-	split := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	    return bufio.ScanWords(data, atEOF)
-	}
-
-	scanner.Split(split)
+	scanner.Split(bufio.ScanWords)
 
 	for scanner.Scan() {
         wordCounts[scanner.Text()]++
