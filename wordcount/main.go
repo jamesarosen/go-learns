255db0b4ee134d2e329dc66bd90b2dbdbded8034
James A Rosen <james@fastly.com>
2017-07-28 10:03:27 -0700

    wordcount: refactor to function

--- wordcount/main.go
+++ wordcount/main.go
@@ -6,18 +6,20 @@ import (
 	"os"
 )
 
-func main() {
+func countWords(r Reader) map[string]int {
 	wordCounts := map[string]int{}
-
 	// adapted from https://golang.org/pkg/bufio/#example_Scanner_custom
-	scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanWords)
 
 	for scanner.Scan() {
         wordCounts[scanner.Text()]++
 	}
+	return wordCounts
+}
 
-	for k, v := range wordCounts {
+func main() {
+	for k, v := range countWords(os.Stdin) {
 		fmt.Printf("%s: %d\n", k, v)
 	}
 }
